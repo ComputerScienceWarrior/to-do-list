328a364be2587829bba6b4036c72dc73ed6eab89
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-12-10 11:27:33 -0700

    create logic for Listitem component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,23 @@
-import ListItem from "./ListItem";
 import React from "react";
+import { items } from "../constants";
+import ListItem from "./ListItem";
 
 const List = (props) => {
     return(
         <div>
-        
+            {
+                Object.values(items).map((item, key) => {
+                    return(
+                        <ListItem 
+                            key={key}
+                            title={item.title}
+                            startDate={item.startDate}
+                            dueDate={item.dueDate}
+                            priorityLevel={item.priorityLevel}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
