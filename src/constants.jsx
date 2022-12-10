b71fabb4b4333540d96f35966403e453db244ce1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-12-10 10:46:30 -0700

    Create constants to display

---
+++ src/constants.jsx
@@ -0,0 +1,37 @@
+export const items = {
+    0: {
+        "title": "Item 1",
+        "startDate": "12/10/2022",
+        "dueDate": "12/12/2022",
+        "completed": false,
+        "priorityLevel": "yellow"
+    },
+    1: {
+        "title": "Item 2",
+        "startDate": "12/09/2022",
+        "dueDate": "12/10/2022",
+        "completed": false,
+        "priorityLevel": "red"
+    },
+    2: {
+        "title": "Item 3",
+        "startDate": "12/07/2022",
+        "dueDate": "12/14/2022",
+        "completed": false,
+        "priorityLevel": "green"
+    },
+    3: {
+        "title": "Item 4",
+        "startDate": "12/06/2022",
+        "dueDate": "12/15/2022",
+        "completed": false,
+        "priorityLevel": "yellow"
+    },
+    4: {
+        "title": "Item 5",
+        "startDate": "12/05/2022",
+        "dueDate": "12/11/2022",
+        "completed": false,
+        "priorityLevel": "green"
+    },
+}
