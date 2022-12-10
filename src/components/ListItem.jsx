743bcb5ba7093aa9d8c5d2d5525f4a2cfa9a2cb5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-12-10 11:27:58 -0700

    Create Logic for ListItem component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListItem = (props) => {
+    const [completed, setCompleted] = useState(props.completed)
+
     return(
-        <div>
-            List Item
+        <div className={completed ? "border-2 border-solid border-green-500 p-10 opacity-25" : "border-2 border-solid border-red-700 p-10"}>
+            <h1>{props.title}</h1>
+            <p><span>{props.startDate}</span><span>{props.dueDate}</span></p>
+            <div>
+            <p>{completed ? "Mark Incomplete" : "Mark Complete"}</p>
+            <button className="bg-indigo-500 p-2 rounded" onClick={() => setCompleted(!completed)}>{completed ? "Incomplete" : "Complete"}</button>
+            </div>
         </div>
     )
 };
