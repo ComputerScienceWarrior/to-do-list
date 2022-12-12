4a3d074593d96ce29b79cec85c0187724b5df315
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-12-11 20:08:18 -0700

    fix date display

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -6,7 +6,8 @@ const ListItem = (props) => {
     return(
         <div className={completed ? "border-2 border-solid border-green-500 p-10 opacity-25" : "border-2 border-solid border-red-700 p-10"}>
             <h1>{props.title}</h1>
-            <p><span>{props.startDate}</span><span>{props.dueDate}</span></p>
+            <p><span>Start Date- {props.startDate}</span></p>
+            <p><span>Due Date- {props.dueDate}</span></p>
             <div>
             <p>{completed ? "Mark Incomplete" : "Mark Complete"}</p>
             <button className="bg-indigo-500 p-2 rounded" onClick={() => setCompleted(!completed)}>{completed ? "Incomplete" : "Complete"}</button>
